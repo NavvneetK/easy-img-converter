365b6a68415982f5875166955f8efc8734ad8c65
Navneet Kaur <navneet78141@gmail.com>
2025-04-14 12:55:35 +0530

    setup.py changed

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name="bulkimgconvert",  # Name of your package
     version="1.0.0",  # Version number
     description="A CLI tool to bulk convert and resize images",  # Short description of your tool
+    long_description=open('README.md').read(),  # Long description from your README file
+    long_description_content_type="text/markdown",  # Ensure the description is treated as markdown
     author="Navneet Kaur",  # Your name
     author_email="navi24493@gmail.com",  # Your email address
     packages=find_packages(),  # Automatically finds packages in your project
     install_requires=[  # Dependencies
-        "Pillow",
-          "tqdm"  # Required library for image processing
+        "Pillow",  # Library for image processing
+        "tqdm",  # Library for progress bars
     ],
     entry_points={  # Define command line tools
         'console_scripts': [
@@ -24,4 +24,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',  # Minimum Python version required
+    include_package_data=True,  # To include any files defined in MANIFEST.in
 )
