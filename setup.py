22250ea5da13f5e9523514fee1fc7ab19cc0e383
Navneet Kaur <navneet78141@gmail.com>
2025-04-14 13:57:24 +0530

    final commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="bulkimgconvert",  # Name of your package
     version="1.0.0",  # Version number
     description="A CLI tool to bulk convert and resize images",  # Short description of your tool
-    long_description=open('README.md').read(),  # Long description from your README file
+    long_description=open('README.md', encoding='utf-8').read(),  # Long description from your README file
     long_description_content_type="text/markdown",  # Ensure the description is treated as markdown
     author="Navneet Kaur",  # Your name
     author_email="navi24493@gmail.com",  # Your email address
